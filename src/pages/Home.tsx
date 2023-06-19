9074a857596500f36ae0bab48fa7ae31ac3025db
Ruan Pelissoli <ruanpelissoli@gmail.com>
2023-06-19 20:49:56 -0300

    add tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,12 @@ import Filter from "../components/Filter";
 const HomePage: React.FC = () => {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="bg-red-500">
+        Teste
+      </p>
       <Filter />
       
     </>
