9debc99f2f35a8ba78924adde0da5c3c059c7dd1
Ramon Pelissoli <ramonpelissoli4@gmail.com>
2023-06-26 17:11:18 -0300

    result detail att

--- src/pages/ResultDetail.tsx
+++ src/pages/ResultDetail.tsx
@@ -9,6 +9,7 @@ const ResultDetailPage: React.FC = () => {
   const [imovel, setImovel] = useState<DetailedImovel | null>(null);
   const [startIndex, setStartIndex] = useState<number>(0);
   const [endIndex, setEndIndex] = useState<number>(2);
+  
 
   useEffect(() => {
     const fetchImoveis = async () => {
@@ -25,7 +26,7 @@ const ResultDetailPage: React.FC = () => {
     };
     fetchImoveis();
   }, [id]);
-  
+
   const goToPreviousImage = () => {
     if (currentImageIndex > 0) {
       setCurrentImageIndex(currentImageIndex - 1);
@@ -55,20 +56,20 @@ const ResultDetailPage: React.FC = () => {
   if (!imovel) {
     return <div>Loading...</div>;
   }
-  
+
   const recomended: Imovel[] = imovel.similarProperties;
-  
+
   return (
-    <div className="flex flex-col justify-between items-start">
-      <div className="flex justify-center items-center fixed rounded-lg h-[800px] w-2/5 ml-6 bg-black shadow shadow-black">
-        <div className="relative items-center justify-center m-w-[600px]">
+    <div className="grid grid-cols-2 gap-4">
+      <div className="flex w-[90%] justify-center relative items-center rounded-lg h-[800px] bg-black shadow shadow-black ml-10">
+        <div className="relative items-center justify-center w-full">
           {imovel.images &&
             imovel.images.map((imagem, index) => (
               <img
                 key={index}
                 src={imagem}
                 alt={imovel.description}
-                className="block m-auto object-contain rounded-lg shadow shadow-black max-w-[80%] max-h-[80%]"
+                className="block m-auto object-contain rounded-lg shadow shadow-black max-w-[650px] max-h-[750px]"
                 style={{
                   display: index === currentImageIndex ? "block" : "none",
                 }}
@@ -99,8 +100,8 @@ const ResultDetailPage: React.FC = () => {
         </div>
       </div>
 
-      <div className="w-6/12 fixed right-0 bg-[ebebeb] shadow shadow-black rounded-sm py-4 px-2.5 h-fit">
-        <h2 className="items-center text-4xl">{imovel.title}</h2>
+      <div className="bg-[ebebeb] shadow shadow-black rounded-sm py-4 px-2.5 h-fit mr-10">
+        <h2 className="items-center text-4xl mb-10">{imovel.title}</h2>
         <p className="text-base mb-4 text-justify indent-2.5">
           {imovel.description}
         </p>
@@ -109,7 +110,7 @@ const ResultDetailPage: React.FC = () => {
         </p>
         <h3 className="text-2xl text-green-600">{imovel.price}</h3>
 
-        <table className="w-3/4 my-2 mx-auto p-0.5 table-fixed">
+        <table className="w-3/4 my-10 mx-auto p-0.5 table-fixed">
           <tbody>
             <tr>
               <th className="w-3/12 bg-[#c5c4c4]">Quartos</th>
@@ -126,148 +127,80 @@ const ResultDetailPage: React.FC = () => {
           </tbody>
         </table>
 
-        <p>
-          <Link to=".." relative="path">
-            Voltar
-          </Link>
-        </p>
-
         <Link
           to={imovel.url}
           target="_blank"
-          className="bg-black text-white rounded-sm no-underline py-2.5 px-5 hover:bg-[#929292]"
+          className="bg-black text-white rounded-sm no-underline py-2.5 px-5 hover:bg-[#929292] mx-6 my-6"
         >
           Ir para o anúncio
         </Link>
-      </div>
 
+        <Link
+          to=".."
+          relative="path"
+          className="bg-black text-white rounded-sm no-underline py-2.5 px-5 hover:bg-[#929292] mx-6 my-6"
+        >
+          Voltar
+        </Link>
 
-
-
-
-      
-
-      <div className="flex flex-row w-3/5 bg-[ebebeb] shadow shadow-black rounded-sm h-[500px] fixed bottom-0 right-0  border-black border-2">
-        <div className="flex flex-row ">
-          {recomended.slice(startIndex, endIndex + 1).map((imovel: Imovel) => (
-            <li
-              key={imovel.id}
-              className="flex justify-center border-solid border-black border-2 flex-col rounded-lg max-w-xs h-[450px] shadow shadow-black my-5 mx-10"
-            >
-              <div className="w-full h-20">
-                <h2 className="mt-0 items-center text-black text-xl uppercase">
-                  {imovel.title}
-                </h2>
-              </div>
-
-              <div className="flex flex-row justify-center items-center m-auto relative w-full h-60 bg-[#5c5c5c]">
-                <div className="max-w-{250px} max-h-{180px}">
-                  {imovel.images.map((imagem, index) => (
-                    <img
-                      key={index}
-                      src={imagem}
-                      alt={imagem}
-                      className="m-auto block object-contain rounded-lg shadow shadow-black max-w-{250px} max-h-{180px}"
-                      style={{
-                        display: index === currentImageIndex ? "block" : "none",
-                      }}
-                    />
-                  ))}
-                </div>
-
-                <button
-                  className={
-                    "absolute top-2/4 translate-y-1/2 text-2xl text-white bg-[#5c5c5c67] py-2 px-3.5 cursor-pointer left-0 hover:opacity-50"
-                  }
-                  onClick={goToPreviousImage}
-                  style={{
-                    display: currentImageIndex === 0 ? "none" : "block",
-                  }}
+        <h2 className="text-xl mt-10">Imóveis Recomendados:</h2>
+        <div className="flex relative justify-center items-center bg-[ebebeb] shadow shadow-black rounded-xl h-[350px] border-black border-2 my-4 mx-11">
+          <div className="flex flex-row space-x-12">
+            {recomended
+              .slice(startIndex, endIndex + 1)
+              .map((imovel: Imovel) => (
+                <li
+                  key={imovel.id}
+                  className="flex justify-center border-solid border-black border-2 flex-col rounded-lg w-[200px] h-[300px] hover:shadow-xl hover:shadow-black my-5"
                 >
-                  &lt;
-                </button>
-                <button
-                  className={
-                    "absolute top-2/4 translate-y-1/2 text-2xl text-white bg-[#5c5c5c67] py-2 px-3.5 cursor-pointer right-0 hover:opacity-50"
-                  }
-                  onClick={goToNextImage}
-                  style={{
-                    display:
-                      currentImageIndex === imovel.images.length - 1
-                        ? "none"
-                        : "block",
-                  }}
-                >
-                  &gt;
-                </button>
-              </div>
-              <div className="flex flex-wrap w-full h-12">
-                {imovel.filterType && (
-                  <div className="flex justify-center items-center p-0.5 max-w-1200px} max-h-{30px} my-0.5 mx-1 text-yellow-200 bg-black rounded-lg text-base">
-                    {imovel.filterType.charAt(0).toUpperCase() +
-                      imovel.filterType.slice(1).toLowerCase()}
+                  <div className="w-full h-20">
+                    <h2 className="mt-0 items-center text-black text-base uppercase overflow-hidden">
+                      {imovel.title}
+                    </h2>
                   </div>
-                )}
-                {imovel.filterBedrooms > 0 && (
-                  <div className="flex justify-center items-center p-0.5 max-w-1200px} max-h-{30px} my-0.5 mx-1 text-yellow-200 bg-black rounded-lg text-base">
-                    {imovel.filterBedrooms === 1
-                      ? `${imovel.filterBedrooms} Quarto`
-                      : `${imovel.filterBedrooms} Quartos`}
-                  </div>
-                )}
-                {imovel.filterBathrooms > 0 && (
-                  <div className="flex justify-center items-center p-0.5 max-w-1200px} max-h-{30px} my-0.5 mx-1 text-yellow-200 bg-black rounded-lg text-base">
-                    {imovel.filterBathrooms === 1
-                      ? `${imovel.filterBathrooms} Banheiro`
-                      : `${imovel.filterBathrooms} Banheiros`}
-                  </div>
-                )}
-                {imovel.filterGarageSpaces > 0 && (
-                  <div className="flex justify-center items-center p-0.5 max-w-1200px} max-h-{30px} my-0.5 mx-1 text-yellow-200 bg-black rounded-lg text-base">
-                    {imovel.filterGarageSpaces === 1
-                      ? `${imovel.filterGarageSpaces} Garagem`
-                      : `${imovel.filterGarageSpaces} Garagens`}
-                  </div>
-                )}
-                {imovel.filterSquareFoot > 0 && (
-                  <div className="flex justify-center items-center p-0.5 max-w-1200px} max-h-{30px} my-0.5 mx-1 text-yellow-200 bg-black rounded-lg text-base">
-                    {imovel.filterSquareFoot}m²
-                  </div>
-                )}
-              </div>
 
-              <div className="w-full h-12">
-                <Link
-                  to={`/results/${imovel.id}`}
-                  className="flex justify-center items-center bg-black no-underline text-white rounded-sm pt-2.5 pb-2.5 mt-2.5 hover:bg-[#707070]"
-                >
-                  Mais Detalhes
-                </Link>
-              </div>
-              <div className="w-full h-14">
-                <h3 className="text-2xl text-green-600">{imovel.price}</h3>
-              </div>
-              
-            </li>
-          ))}
-          
+                  <div className="flex flex-row justify-center items-center m-auto relative w-full h-60">
+                    <div className="max-w-full max-h-[150px]">
+                      <img
+                        src={imovel.images[0]}
+                        alt={imovel.images[0]}
+                        className="m-auto block object-contain rounded-lg shadow shadow-black max-w-full max-h-[150px]"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="w-full h-14">
+                    <h3 className="text-2xl text-green-600">{imovel.price}</h3>
+                  </div>
+
+                  <div className="w-full h-12 mb-0">
+                    <Link
+                      to={`/results/${imovel.id}`}
+                      className="flex justify-center items-center bg-black no-underline text-white rounded-sm py-1.5  hover:bg-[#707070]"
+                    >
+                      Mais Detalhes
+                    </Link>
+                  </div>
+                </li>
+              ))}
+          </div>
+          <button
+            className="absolute top-[120px] translate-y-1/2 text-5xl text-[#0000004d] cursor-pointer left-0 hover:text-black"
+            onClick={goToPreviousProperties}
+            style={{ display: startIndex === 0 ? "none" : "block" }}
+          >
+            &lt;
+          </button>
+          <button
+            className="absolute top-[120px] translate-y-1/2 text-5xl text-[#0000004d] cursor-pointer right-0 hover:text-black"
+            onClick={goToNextProperties}
+            style={{
+              display: endIndex === recomended.length - 1 ? "none" : "block",
+            }}
+          >
+            &gt;
+          </button>
         </div>
-        <button
-          className="absolute top-2/4 translate-y-1/2 text-2xl text-white bg-[#5c5c5c67] py-2 px-3.5 cursor-pointer left-0 hover:opacity-50"
-          onClick={goToPreviousProperties}
-          style={{ display: startIndex === 0 ? "none" : "block" }}
-        >
-          &lt;
-        </button>
-        <button
-          className="absolute top-2/4 translate-y-1/2 text-2xl text-white bg-[#5c5c5c67] py-2 px-3.5 cursor-pointer right-0 hover:opacity-50"
-          onClick={goToNextProperties}
-          style={{
-            display: endIndex === recomended.length - 1 ? "none" : "block",
-          }}
-        >
-          &gt;
-        </button>
       </div>
     </div>
   );
